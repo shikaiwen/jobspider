63996d4143c2b256b912518fc1136253b4a8f3d9
kevin <googlevsbing@126.com>
2017-02-27 17:17:00 +0800

    完成城市爬虫，但程序性能很差，时间应该是消耗在List的扩容了，代码待优化

--- src/main/java/com/kevin/db/MongoConnector.java
+++ src/main/java/com/kevin/db/MongoConnector.java
@@ -1,27 +1,94 @@
 package com.kevin.db;
 
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 /**
  * Created by kaiwen on 27/02/2017.
+ * http://www.runoob.com/mongodb/mongodb-query.html
+ * http://www.runoob.com/mongodb/mongodb-java.html
+ *
+ *  vim /etc/mongod.conf
+ *
+ * centos
+ *  service mongod start
+ *  service mongod stop
+ *
+ * server: centOS 6.6
+ *
+ * vim /etc/yum.repos.d/mongodb-org-3.4.repo
+     [mongodb-org-3.4]
+     name=MongoDB Repository
+     baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+     gpgcheck=1
+     enabled=1
+     gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+ *
+ * sudo yum install -y mongodb-org
+ *
+ * install log :
+ * Installed:
+ mongodb-org.x86_64 0:3.4.2-1.el6
+ Dependency Installed:
+ mongodb-org-mongos.x86_64 0:3.4.2-1.el6
+ mongodb-org-server.x86_64 0:3.4.2-1.el6
+ mongodb-org-shell.x86_64 0:3.4.2-1.el6
+ mongodb-org-tools.x86_64 0:3.4.2-1.el6
+ *
+ *
+ *
+ * op command:
+ * show dbs
+ * use dbname
+ * db.COLLECTION_NAME.find()
+ *
  */
 public class MongoConnector {
 
+    public static void main(String[] args) {
+//        getDB("test");
+        new MongoConnector().insert();
+    }
+
     public static MongoDatabase getDB(String db){
 //        Mongo mongo = new Mongo("localhost", 27017);
-        MongoClient client = new MongoClient("localhost", 27017);
+        MongoClient client = new MongoClient("172.16.2.31", 27017);
         MongoDatabase database = client.getDatabase(db);
         return database;
     }
 
+    public MongoClient getClient(){
+        MongoClient client = new MongoClient("172.16.2.31", 27017);
+        return client;
+    }
+
+
+
+
+
+
 
     public void insert(){
-        MongoDatabase test = getDB("test");
 
-        MongoCollection<Document> province = test.getCollection("province");
+//        MongoDatabase db = getDB("test");
+//        MongoCollection<Document> test = db.getCollection("test");
+//        Document document = new Document();
+//        document.append("username", "kevin")
+//                .append("age", "18");
+//        test.insertOne(document);
+
+        MongoClient client = this.getClient();
+        DB db = client.getDB("test");
+
+        BasicDBObject dbObject = new BasicDBObject();
+        dbObject.append("username", "kevin")
+                .append("age", "18");
+
+        DBCollection emp = db.getCollection("emp");
+        WriteResult insert = emp.insert(dbObject);
+
 
     }
 }
--- src/main/java/com/kevin/spider/ProvinceCityCountySpider.java
+++ src/main/java/com/kevin/spider/ProvinceCityCountySpider.java
@@ -1,10 +1,14 @@
 package com.kevin.spider;
 
-import com.alibaba.fastjson.JSON;
+import com.kevin.db.MongoConnector;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -15,9 +19,111 @@ public class ProvinceCityCountySpider {
 
     public static void main(String[] args) {
         ProvinceCityCountySpider spider = new ProvinceCityCountySpider();
-        Map map = spider.getProvince().get(0);
-        spider.getCityList(map.get("nextUrl").toString());
+//        Map map = spider.getProvince().get(0);
+//        spider.getCityList(map.get("nextUrl").toString());
+
+        spider.insertData();
     }
+
+
+    public void insertData(){
+
+        MongoConnector connector = new MongoConnector();
+        List<Map> province = getProvince();
+
+        MongoDatabase test = connector.getClient().getDatabase("test");
+        MongoCollection<org.bson.Document> regionCol = test.getCollection("region");
+
+//        List provinceDocList = new ArrayList();
+//        province.forEach((item)->{
+//            org.bson.Document doc = new org.bson.Document();
+//            doc.putAll(item);
+//            doc.put("type", 1);
+//            provinceDocList.add(doc);
+//        });
+
+        List proDocList = new ArrayList();
+        List cityDocList = new ArrayList();
+        List countyDocList = new ArrayList();
+        List townDocList = new ArrayList();
+        List villageDocList = new ArrayList();
+
+
+        province.forEach((promap)->{
+            org.bson.Document doc = new org.bson.Document();
+            doc.put("type", 1);
+            doc.putAll(promap);
+            proDocList.add(doc);
+
+            String nextUrl =  (String)promap.get("nextUrl");
+            if(StringUtils.isEmpty(nextUrl)) return;
+
+            List<Map> cityList = getCityList(nextUrl);
+            cityList.forEach((cityMap)->{
+
+                org.bson.Document doc2 = new org.bson.Document();
+                doc2.putAll(cityMap);
+                doc2.put("type", 2);
+                doc2.put("parent", promap.get("code"));
+                cityDocList.add(doc2);
+
+                String cityNextUrl =  (String)cityMap.get("nextUrl");
+                if(StringUtils.isEmpty(cityNextUrl)) return;
+                List<Map> countyList = getCountyList(cityNextUrl);
+                countyList.forEach((countyMap)->{
+
+                    org.bson.Document doc3 = new org.bson.Document();
+                    doc3.putAll(countyMap);
+                    doc3.put("type", 3);
+                    doc3.put("parent", cityMap.get("code"));
+                    countyDocList.add(doc3);
+
+                    String countyNextUrl =  (String)countyMap.get("nextUrl");
+                    if(StringUtils.isEmpty(countyNextUrl)) return;
+                    List<Map> townList = getTownList(countyNextUrl);
+
+                    townList.forEach((townMap)->{
+
+
+                        org.bson.Document doc4 = new org.bson.Document();
+                        doc4.putAll(townMap);
+                        doc4.put("type", 4);
+                        doc4.put("parent", countyMap.get("code"));
+                        townDocList.add(doc4);
+
+                        String townNextUrl =  (String)townMap.get("nextUrl");
+                        if(StringUtils.isEmpty(townNextUrl)) return;
+                        List<Map> villageList = getVillageList(townNextUrl);
+
+                        villageList.forEach((villageMap)->{
+
+                            org.bson.Document doc5 = new org.bson.Document();
+                            doc5.putAll(villageMap);
+
+                            doc5.put("type", 5);
+                            doc5.put("parent", townMap.get("code"));
+                            villageDocList.add(doc5);
+
+                        });
+
+                    });
+
+                });
+
+            });
+
+        });
+
+
+//        regionCol.insertMany(provinceDocList);
+
+        regionCol.insertMany(proDocList);
+        regionCol.insertMany(cityDocList);
+        regionCol.insertMany(countyDocList);
+        regionCol.insertMany(townDocList);
+        regionCol.insertMany(villageDocList);
+    }
+
 
     public Document getDocument(String url){
         Document doc = null;
@@ -25,6 +131,7 @@ public class ProvinceCityCountySpider {
             doc = Jsoup.connect(url).get();
         } catch (IOException e) {
             e.printStackTrace();
+            return getDocument(url);
         }
 //        Elements newsHeadlines = doc.select("#mp-itn b a");
 
@@ -37,27 +144,19 @@ public class ProvinceCityCountySpider {
     }
 
 
-    public List getVillageList(String url) {
+    public List<Map> getVillageList(String url) {
 
         List countyList = new ArrayList();
         Document document = getDocument(url);
         Elements cityTrs = document.select(".villagetable .villagetr");
         for (Iterator<Element> iterator = cityTrs.iterator(); iterator.hasNext(); ) {
-            Element next = iterator.next();
-            Element codeTd = next.child(0);
-            String code = codeTd.child(0).text();
-
-            Element nameTd = next.child(1);
-
-            String name = nameTd.child(0).text();
-            String href = nameTd.child(0).attr("href");
-            String nextUrl = getUrlPath(url) + href;
+            Element tr = iterator.next();
+            String code = tr.child(0).text();
+            String name = tr.child(2).text();
 
             Map map = new HashMap();
             map.put("name", name);
             map.put("code", code);
-            map.put("nextUrl", nextUrl);
-
             countyList.add(map);
 
         }
@@ -93,17 +192,23 @@ public class ProvinceCityCountySpider {
         return countyList;
     }
 
-    public List getCountyList(String url) {
+    public List<Map> getCountyList(String url) {
 
         List countyList = new ArrayList();
         Document document = getDocument(url);
         Elements cityTrs = document.select(".countytable .countytr");
         for (Iterator<Element> iterator = cityTrs.iterator(); iterator.hasNext(); ) {
-            Element next = iterator.next();
-            Element codeTd = next.child(0);
-            String code = codeTd.child(0).text();
 
+            System.out.println(url);
+
+            Element next = iterator.next();
+//            Element codeTd = next.child(0);
+            if(next.children().size()<2) continue;
+            String code = next.child(0).text();
             Element nameTd = next.child(1);
+
+
+            if(nameTd.children().size() == 0) continue;
 
             String name = nameTd.child(0).text();
             String href = nameTd.child(0).attr("href");
@@ -124,7 +229,7 @@ public class ProvinceCityCountySpider {
 
 
 
-    public List getCityList(String url) {
+    public List<Map> getCityList(String url) {
 
         List cityList = new ArrayList();
         Document document = getDocument(url);
