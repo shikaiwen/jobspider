64e8496053f4372678a03e5b2aef114f0bef7e1e
kevin <googlevsbing@126.com>
2017-03-10 17:46:13 +0800

    lamdatest

---
+++ src/test/java/LamdaTest.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by kaiwen on 10/03/2017.
+ *
+ * 学习lamda表达式：http://blog.oneapm.com/apm-tech/226.html
+ * http://viralpatel.net/blogs/Lambda-expressions-java-tutorial/
+ */
+public class LamdaTest {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public void semicolonTest(){
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        list.forEach(n -> System.out.println(n));
+
+        //or we can use :: double colon operator in Java 8
+        list.forEach(System.out::println);
+        StringBuffer buffer = new StringBuffer();
+        list.forEach(buffer::append);
+//        new StringBuffer()::append
+
+    }
+
+
+    public static void test1(){
+
+        LamdaTest lamdaTest = new LamdaTest();
+        lamdaTest.execute(()->{
+
+        });
+    }
+
+    public void execute(WorkerInterface workerInterface){
+
+        workerInterface.doSomeWork();
+    }
+
+
+    public void basicInterface(){
+        Thread t = new Thread(()->{
+
+        });
+
+        Consumer<Integer> c = (Integer x) ->{
+
+        };
+        new ArrayList <Integer>().forEach(c);
+    }
+}
+
+@FunctionalInterface
+interface WorkerInterface{
+    public void doSomeWork();
+}
